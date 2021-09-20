8cdb66792225acf523090f92662694fc8c155ee7
Valentyn Chubukin <val@valch.name>
2021-09-20 23:45:59 +0300

    submit write-a-function

---
+++ write-a-function.py
@@ -0,0 +1,16 @@
+def is_leap(year):
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                leap = True
+            else:
+                leap = False
+        else:
+            leap = True
+    else:
+        leap = False
+    
+    print(leap)
+
+year = int(1990)
+is_leap(year)
